7736d7d5c4a13e097ace987eaf6d266c094e08cc
edubew <edubew98@gmail.com>
2023-06-09 12:04:17 +0300

    Write  validations for users and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # Validations
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # Method to update the posts counter for a user.
   def update_posts_counter
     user.update(posts_counter: user.posts.count)
