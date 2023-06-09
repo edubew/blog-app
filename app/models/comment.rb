e684ffda0c45a2bcb0f1e569337af239eba61717
edubew <edubew98@gmail.com>
2023-06-09 07:21:17 +0300

    Fix linters errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    # Method to update comments counter for a post
-    def update_posts_comments_counter
-        post.update(comments_counter: posts.comments.count)
-    end
+  # Method to update comments counter for a post
+  def update_posts_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    # Method to update likes counter for a post
-    def update_post_likes_counter
-        post.update(likes_counter: post.likes.count)
-    end
+  # Method to update likes counter for a post
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
