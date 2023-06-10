7fa9458b179c1036364970355fe04b8d5643ad6b
edubew <edubew98@gmail.com>
2023-06-10 18:38:59 +0300

    Write unit test for post.rb associations, methods and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
 
   # Validations
-  validates :title, presence: true, length: { maximum: 250 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   # Method to update the posts counter for a user.
-  def update_posts_counter
+  def self.update_posts_counter(user_id)
+    user = User.find(user_id)
     user.update(posts_counter: user.posts.count)
   end
 
