b031dccbc336bc4c909cfe146d51634e1deca28a
edubew <edubew98@gmail.com>
2023-06-10 18:24:37 +0300

    Write unit test for user.rb associations, methods and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe User, type: :model do
         it { should have_many(:comments)}
         it { should have_many(:posts)}
     end
-end
+
+    describe 'validations' do
+        it { should validate_presence_of(:name) }
+        it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
+    end
+
+    describe 'recent_posts' do
+        let(:user) { User.create!(name: 'Winnie', posts_counter: 0) }
+
+        it 'returns an array if the user has no posts' do 
+            recent_posts = user.recent_posts
+
+            expect(recent_posts).to eq([])
+        end
+    end
+
+    end
