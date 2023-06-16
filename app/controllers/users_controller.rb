d3a92e3fe757c834811b70570d47483df9b4148b
edubew <edubew98@gmail.com>
2023-06-16 05:16:21 +0300

    Setup users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    def index
+        # Retrieve all users from a db
+    end
+
+    def show
+        # Retrieve a specific user from the db
+    end
+end
