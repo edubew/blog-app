09303b0e3f5a75a2aa09b148489e057f5cc807ef
edubew <edubew98@gmail.com>
2023-06-16 05:16:05 +0300

    Setup posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsContoller < ApplicationController
+    def index
+        # Retrieve all posts from the db
+    end
+
+    def show
+        # Retrive a specific post from the db
+    end
+end
