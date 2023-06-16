c8eefe2687e7143ee91ef37a5abea6d9b30265b9
edubew <edubew98@gmail.com>
2023-06-16 16:49:48 +0300

    Fix linters errors

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -1,19 +1,19 @@
 require 'rspec/rails'
 
 RSpec.describe 'Posts', type: :request do
-    describe "GET /index" do
-        it "Renders a successful response" do
-            get posts_path
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include("A list of all posts for a given user")
-        end
+  describe 'GET /index' do
+    it 'Renders a successful response' do
+      get posts_path
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('A list of all posts for a given user')
     end
+  end
 
-    describe "GET /show" do
-        it "Renders a successful response" do
-            get "/posts/:id"
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include("A specific post from the database")
-        end
+  describe 'GET /show' do
+    it 'Renders a successful response' do
+      get '/posts/:id'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('A specific post from the database')
     end
+  end
 end
