f4ded737442babdf9be59d5aeb3661fad29f0694
edubew <edubew98@gmail.com>
2023-06-17 21:40:40 +0300

    Write a method to create a post on behalf of the current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(@user), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
